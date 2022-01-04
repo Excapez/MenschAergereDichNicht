24fb30eef2e2d560c6f48b3e2e1371b73bff9aa7
yyiann3s <yanik.yiannakis@smail.emt.h-brs.de>
2022-01-04 17:10:04 +0100

    Gui Klasse hinzugefügt.2

---
+++ src/com/company/Gui.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Gui {
+	
+	int startfelder1[] = { 0, 1, 7, 8};
+	int startfelder2[] = { 5, 6,12,13};
+	int startfelder3[] = {58,59,65,66};
+	int startfelder4[] = {63,64,70,71};
+	
+	int zielfelder1[] = {32,33,34,35};
+	int zielfelder2[] = {10,15,18,25};
+	int zielfelder3[] = {39,38,37,36};
+	int zielfelder4[] = {61,56,53,46};
+	
+	int lauffelder[] = {20, 21, 22, 23, 24, 17, 14,  9,  2,  3,  // Ecke Spieler 1
+						 4, 11, 16, 19, 26, 27, 28, 29, 30, 40,  // Ecke Spieler 2
+						51, 50, 49, 48, 47, 54, 57, 62, 69, 68,  // Ecke Spieler 3
+						67, 60, 55, 52, 45, 44, 43, 42, 41, 31}; // Ecke Spieler 4 
+}
